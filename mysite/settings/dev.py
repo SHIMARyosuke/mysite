c4df27c26eedad943970c2dc93a5bd669adbada4
SHIMA Ryosuke <shima@unalus.com>
2022-08-18 17:35:49 +0900

    initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f37^+iol+bzl_#87yxx+o*-46zs%25#4j++bq)u2y@a8)!ffp("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
